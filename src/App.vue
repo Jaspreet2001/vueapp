d2a10fd5ff68f9232c71fca2d330d578acbe829f
Jaspreet2001 <jaspreetjawanda06@gmail.com>
2024-03-24 19:20:37 +0530

    feat:blog app where we add product

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,104 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AddProduct></AddProduct>
+  <h1 style="text-align: center; border:3px; background-color:hsl(192, 82%, 78%); border: 1px solid #ccc;
+  border-color: #3608ed;border-width:3px
+  ">DISPLAYING THE PRODUCTS DETAIL</h1>
+<ul class="item" v-for="item in list" :key="item.id">
+  <div class="lis">
+  <li class="re"> ITEM ID => {{item.id}}</li>
+  <li class="re">ITEM TITLE => {{item.title}}</li>
+  <li class="re">ITEM DESCRIPTION => {{item.description}}</li>
+  <li class="re">ITEM PRICE => {{item.price}}</li>
+  <li class="re">ITEM BRAND=> {{item.brand}}</li></div>
+  
+  <div class="im" v-if="item.images">
+ <li class="img" v-for ="res in item.images" :key="res.id">
+  
+  <img class ="img2" :src="res" alt="Image" />
+
+</li>
+</div>
+<button @click="deleteProduct" class="button1">Delete Product</button>
+</ul>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AddProduct from './components/AddProduct.vue'
+import axios from 'axios';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name:'App',
+
+  components:{
+    AddProduct,
+  },
+
+  data(){
+    return{
+    list:[]  
+    }
+  },
+
+  async mounted(){
+    let result=await axios.get("http://mail.zaxtom.com:3000/products");
+    result.data.reverse();
+    console.log("api data",result.data);
+    this.list=result.data;
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.im {
+  justify-content: center;
+  border: 2px solid #ccc;
+  border-color: black;
+  width:"fit-content";
+  list-style: none;
+  display: flex; 
+  flex-wrap: wrap;
+  padding: 10px; 
+  display: flex;
+};
+.img{
+  
+  margin-right: 10px;
+  width:"fit-content";
+  border: 1px solid #ccc;
+  border-color: #de0e15;
+  padding: 10px;
+};
+.red{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 50px;
+  font-family: Arial, Helvetica, sans-serif;
+  text-decoration: #de0e15;
 }
+.lis {
+  list-style-type: none; /* Remove default list styles */
+  padding: 10px;
+  border: 1px solid #ddd; /* Add a border for separation */
+  margin-bottom: 10px; /* Add space between cart items */
+background-color: hwb(60 98% 0%);
+border-color: #de0e15;
+border-width: 3px;
+border-style: dashed;
+}
+
+.lis li {
+  margin-bottom: 5px; /* Add space between list items */
+}
+.re{
+  display:flex;
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: hsl(68, 93%, 89%);
+  padding-bottom: 1px;
+  font-size:medium;
+  font-size:20px;
+  font-style: normal;
+  
+
+}
+
+
 </style>
---
+++ src/components/AddProduct.vue
@@ -0,0 +1,93 @@
+<template>
+    <h1 style="text-align: center; border:3px; background-color:hwb(69 25% 2%); border: 1px solid #ccc;
+    border-color: hsl(0, 94%, 51%);border-width:3px
+    ">ADD BLOG</h1>
+    <div class="fr">
+    <form class="red">
+      <label ></label>
+      <input type="text" name="product" v-model="title"/>
+      <br/>
+    
+      <label >Price        </label>
+      <input type="text" name="price" v-model="price"/>
+      <br/>
+      <label >Description</label>
+      <input type="text" name="description" v-model="description"/>
+      <br/>
+      <label>Brand</label>
+      <input type="text" name="brand" v-model="brand"/>
+      <br/>
+      
+      <button v-on:click="addUser()" class="green">Add User</button>
+    </form>
+</div>
+    </template>
+
+    <script>
+    import axios from 'axios'
+export default{
+      name:'AddProduct',
+      data(){
+        return{
+          title:'',
+          price:'',
+          description:'',
+          brand:'',
+        }
+      },
+      methods:{
+        async addUser(){
+          let result = await axios.post("http://mail.zaxtom.com:3000/products",{
+            title:this.title,
+            price:this.price,
+            description:this.description,
+            brand:this.brand,
+          });
+          console.log(result.data);
+        },
+       
+    }
+}
+</script>
+
+<style>
+
+.red{
+    align-content: center;
+    background-color: rgb(198, 234, 252);
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    width: 300px; /* Adjust the width as needed */
+  } 
+  button{
+    justify-content: center;
+    margin: 10px;
+    padding: 10px;
+    background-color: hwb(249 2% 51%);
+    color: white;
+    border-radius: 5px;
+    border: none;
+    align-content: center;
+    
+  } 
+  label {
+    
+    display: block;
+    margin-bottom: 5px;
+    font-weight: bold;
+  }
+  input {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 10px;
+    box-sizing: border-box;
+  }
+  .fr{
+    justify-content: center;
+margin-left: 580px;
+  }
+  .green{
+    margin-left: 105px;
+  }
+</style>
